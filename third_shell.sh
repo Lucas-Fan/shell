34ff39fed803dfde7a42fd6b6149553fc56bbe6e
Lucas-Fan <2209253706@qq.com>
2019-01-16 21:16:00 +0800

    third

---
+++ third_shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#by authors fzyt 2019
+
+echo "\033[32mPlease select Menu follow:\033[0m"
+
+echo "1)安装apache服务器"
+echo "2)安装Mysql服务器"
+echo "3)安装PHP服务器"
+echo "4)配置LAMP WEB架构"
+
+echo "-----------------------------"
