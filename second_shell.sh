a6088a4a3f4b00afe7b77640c8f943df2cfe6cf2
Lucas-Fan <2209253706@qq.com>
2019-01-16 21:14:05 +0800

    second

---
+++ second_shell.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#define path variables
+#by authors fzyt 2019
+
+
+A=123
+name=fzyt
+
+echo "This is my First shell var $A"
+echo "My name is $name"
+
+echo "$UID"
+
+echo $PWD
+
+echo "##############"
+
+echo $0
+echo $1 $2
+
+echo "The \$1 is $1"
+echo "The \$2 is $2"
+echo "The \$? is $?"
+echo "The \$* is $*"
+echo "The \$# is $#"
+
